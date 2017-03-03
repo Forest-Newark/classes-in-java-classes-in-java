e91b8919cbdb34cb0aa22f78be2b4a01353972cd
Forest Newark <forest.newark@gmail.com>
2017-03-03 12:21:05 -0500

    initial commit

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -2,3 +2,21 @@
  * Create the most minimal class you can. The class must be named "Animal"
  * because it must match the file name.
  */
+
+public class Animal {
+
+    private String mAnimalType;
+    private int mAnimalAge;
+    public Animal(String animalType,int animalAge) {
+        mAnimalType = animalType;
+        mAnimalAge = animalAge;
+
+    }
+    @Override public String toString() {
+       return ("Look its a " + this.mAnimalAge + " year old " + this.mAnimalType + "!" + " How cool!");
+
+    }
+
+
+
+}
